0926628c98159cfe047d692ab7e8f0ab7a1ac5a0
nofx3000 <398622407@qq.com>
2023-01-04 22:09:16 +0800

    index page, add react-redux

--- src/views/Index/Index.tsx
+++ src/views/Index/Index.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Layout } from "antd";
 import style from "./index.module.scss";
+import { Outlet } from "react-router-dom";
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Sider, Content } = Layout;
 
 const App: React.FC = () => {
   return (
     <Layout>
-      <Header>Header</Header>
+      <Header className={style.header}>Header</Header>
       <Layout>
-        <Sider>Sider</Sider>
-        <Content>Content</Content>
+        <Sider className={style.sider}>Sider</Sider>
+        <Content className={style.content}>
+          <Outlet></Outlet>
+        </Content>
       </Layout>
-      <Footer>Footer</Footer>
     </Layout>
   );
 };
