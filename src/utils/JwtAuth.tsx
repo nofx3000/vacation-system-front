26cd099ceefc413b0814bc5ffcfb3545665fc82d
nofx3000 <398622407@qq.com>
2023-01-05 10:18:13 +0800

    before modify jwt auth

--- src/utils/JwtAuth.tsx
+++ src/utils/JwtAuth.tsx
@@ -1,12 +1,22 @@
-import { Navigate } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectToken,
+  selectUserinfo,
+  verifyTokenAsync,
+} from "../store/slices/userinfoSlice";
 
 export default function JwtAuth(props: any) {
+  // 1.浏览器是否有token
+  // 2.redux是否有userinfo
+  // 3.userinfo的exp时间是否过期（exp*1000）
+  // 4.axios——verify
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const token = window.localStorage.getItem("token");
   if (!token) {
-    return <Navigate to="/login" />;
+    navigate("/login");
   }
   axios.defaults.headers["authorization"] = token;
   async function verify() {
